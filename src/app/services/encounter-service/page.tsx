4417a2c7b691bdc1b9285f2b0c297edbbf17ccca
cptcode123 <digwe790@gmail.com>
2025-10-03 13:27:30 +0100

    feat/audio src test

--- src/app/services/encounter-service/page.tsx
+++ src/app/services/encounter-service/page.tsx
@@ -1,17 +1,13 @@
-'use client'
+
 
 import React from "react"
 import HeroStatic from "@/components/ui/HeroStatic"
 import { motion } from "framer-motion"
-import { fetchRecordingsByKeyword, MixlrRecording } from "../../../../lib/mixlr"
+import MediaCard from "@/components/ui/MediaCard"
 
 
-
-export default async function SchoolOfTheSpiritPage() {
-    const feedUrl = 'https://apicdn.mixlr.com/rss-feeds/3017'
-
-    const tuesdayRecordings = await fetchRecordingsByKeyword(feedUrl, 'Encounter God Service', "Encounter-God-Service")
-
+export default function SchoolOfTheSpiritPage() {
+   
     return (
         <div>
             {/* Hero */}
@@ -19,14 +15,10 @@ export default async function SchoolOfTheSpiritPage() {
 
             {/* Content */}
             <section className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-                <motion.h2
-                    className="text-3xl font-extrabold text-gray-900 mb-4 text-center"
-                    initial={{ opacity: 0, y: 20 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    School of the Spirit
-                </motion.h2>
+                <audio controls>
+                    <source src="https://mixlr.com/recording_rss_redirect/65428766-4deb-4431-844a-e2c49fd079c2.mp3" type="audio/mpeg"/>
+                    Your browser does not support the audio element
+                </audio>
             </section>
         </div>
     )
